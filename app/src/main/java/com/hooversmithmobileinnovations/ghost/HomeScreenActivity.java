4267cb2af773104961fb3fb9a6914150923540b6
Andrew Hoover <ahoover11@gmail.com>
2015-03-31 21:28:32 -0400

    Activity Change

--- app/src/main/java/com/hooversmithmobileinnovations/ghost/HomeScreenActivity.java
+++ app/src/main/java/com/hooversmithmobileinnovations/ghost/HomeScreenActivity.java
@@ -1,12 +1,13 @@
 package com.hooversmithmobileinnovations.ghost;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeScreenActivity extends ActionBarActivity {
+public class HomeScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
